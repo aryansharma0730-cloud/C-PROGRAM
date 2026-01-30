e9a1428b62bbcd59516df6a1273caedc7a08a25c
aryansharma0730-cloud <aryansharma0730@gmail.com>
2026-01-30 12:45:05 +0530

    Add files via upload

---
+++ exp4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int radius;
+    scanf("%d",&radius);
+    int area=radius*radius;
+    int circum=2*3.14*radius;
+    printf("%d\n",area);
+    printf("%d\n",circum);
+    return 0;
+}
